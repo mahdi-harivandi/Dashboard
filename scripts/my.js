0fe0bba83edd1e1ddc3415d7102432b9ca569473
mahdi-harivandi <harivandiprogramming@gmail.com>
2024-08-29 11:18:53 -0700

    Modify the menu button and add my.js

---
+++ scripts/my.js
@@ -0,0 +1,10 @@
+// navbar 
+$(document).ready(function(){
+    $(".nav_button").click(function(){
+        $(".nav_button").toggleClass("change")
+    })
+})
+// tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
